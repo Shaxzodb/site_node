94f67a3e9121eeaa600c5f53b18f46a072748926
Shaxzodb <shaxzodbmaster@gmail.com>
2022-10-13 19:04:55 +0500

    Site Add

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,27 @@
 import express from "express";
-//import helmet from "helmet";
+import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
-//import compression from "compression";
+import compression from "compression";
 import cookieParser from "cookie-parser";
-// import ejs from 'ejs';
+import ejs from 'ejs';
 import methodOverride from "method-override";
 import NotFound from "./middleware/notFound";
 //import corsOptionsDelegate from "./middleware/cors";
 
 import router from "./routes/router";
 const app = express();
-import { createServer } from "node:http";
-const http = createServer(app);
+// import { createServer } from "node:http";
+// const http = createServer(app);
 
 
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-//app.use(compression()); // for compressing response bodies
+app.use(compression()); // for compressing response bodies
 app.use(cookieParser('@K3mMdbME0'));
 app.set("view engine", "ejs");
+app.engine('html', ejs.renderFile);
 app.use("/static",express.static("static"));
 app.use('/uploads/',express.static("uploads"));
 app.use(methodOverride('_method'))
@@ -28,17 +29,17 @@ if (app.get("env") === "development") {
 	app.use(morgan("tiny")); // for logging requests
 	
 	
-} //else {
-	// app.use(helmet()); // for logging and security
-	// app.enable("trust proxy"); // trust proxy is a method of express that allows you to set the value of req.ips to the IP address of the proxy that connected to your app.
-	// app.use((req, res, next) => {
-	// 	if (req.secure) {
-	// 		next();
-	// 	} else {
-	// 		res.redirect("https://" + req.headers.host + req.url);
-	// 	}
-	// });
-//}
+} else {
+	app.use(helmet()); // for logging and security
+	app.enable("trust proxy"); // trust proxy is a method of express that allows you to set the value of req.ips to the IP address of the proxy that connected to your app.
+	app.use((req, res, next) => {
+		if (req.secure) {
+			next();
+		} else {
+			res.redirect("https://" + req.headers.host + req.url);
+		}
+	});
+}
 
 
 // app.use(cors({
@@ -52,6 +53,6 @@ app.use(NotFound);
 
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
+app.listen(PORT, () => {
 	console.log(`Server is starting on port ${PORT}...`);
 });
